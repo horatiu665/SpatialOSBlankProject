57553cc3c9c0c763e9bfca5cb25cda2a68cb8b3c
Rami Ahmed Bock <Rami@ApexGameTools.com>
2017-02-02 13:46:37 +0100

    minor simplification

--- workers/unity/Assets/Gamelogic/PlayerSyncSender.cs
+++ workers/unity/Assets/Gamelogic/PlayerSyncSender.cs
@@ -51,6 +51,11 @@ namespace Assets.Gamelogic
 
         private void FixedUpdate()
         {
+            if (!_playerWriter.HasAuthority)
+            {
+                return;
+            }
+
             var frameCount = Time.frameCount;
             if (frameCount < _lastSend)
             {
@@ -59,43 +64,40 @@ namespace Assets.Gamelogic
 
             _lastSend = frameCount + _sendFrameFrequency;
 
-            if (_playerWriter.HasAuthority)
+            var update = new PlayerComponent.Update();
+            var change = false;
+
+            var newPos = this.transform.position.ToCoordinates();
+            if (newPos.HasChangedBy(_playerWriter.Data.position, _positionSendThreshold))
             {
-                var update = new PlayerComponent.Update();
-                var change = false;
+                update = update.SetPosition(newPos);
+                change = true;
+            }
 
-                var newPos = this.transform.position.ToCoordinates();
-                if (newPos.HasChangedBy(_playerWriter.Data.position, _positionSendThreshold))
-                {
-                    update = update.SetPosition(newPos);
-                    change = true;
-                }
+            var newHead = _head.transform.ToTransformData();
+            if (newHead.HasChangedBy(_playerWriter.Data.head, _positionSendThreshold, _rotationSendThreshold))
+            {
+                update = update.SetHead(newHead);
+                change = true;
+            }
 
-                var newHead = _head.transform.ToTransformData();
-                if (newHead.HasChangedBy(_playerWriter.Data.head, _positionSendThreshold, _rotationSendThreshold))
-                {
-                    update = update.SetHead(newHead);
-                    change = true;
-                }
+            var newLeftHand = _leftHand.transform.ToTransformData();
+            if (newLeftHand.HasChangedBy(_playerWriter.Data.leftHand, _positionSendThreshold, _rotationSendThreshold))
+            {
+                update = update.SetLeftHand(newLeftHand);
+                change = true;
+            }
 
-                var newLeftHand = _leftHand.transform.ToTransformData();
-                if (newLeftHand.HasChangedBy(_playerWriter.Data.leftHand, _positionSendThreshold, _rotationSendThreshold))
-                {
-                    update = update.SetLeftHand(newLeftHand);
-                    change = true;
-                }
+            var newRightHand = _rightHand.transform.ToTransformData();
+            if (newRightHand.HasChangedBy(_playerWriter.Data.rightHand, _positionSendThreshold, _rotationSendThreshold))
+            {
+                update = update.SetRightHand(newRightHand);
+                change = true;
+            }
 
-                var newRightHand = _rightHand.transform.ToTransformData();
-                if (newRightHand.HasChangedBy(_playerWriter.Data.rightHand, _positionSendThreshold, _rotationSendThreshold))
-                {
-                    update = update.SetRightHand(newRightHand);
-                    change = true;
-                }
-
-                if (change)
-                {
-                    _playerWriter.Send(update);
-                }
+            if (change)
+            {
+                _playerWriter.Send(update);
             }
         }
 
